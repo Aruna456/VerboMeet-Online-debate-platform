31ae980838d31fb9d70927b4907795dcae426f38
Aruna456 <aruna.techbox@gmail.com>
2024-10-07 23:04:38 +0530

    Modified UI

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -10,28 +10,28 @@ const Home = () => {
     };
 
     return (
-        <> 
-            <div className="w-screen h-screen overflow-hidden"> 
+        <>
+            <div className="w-screen h-screen overflow-hidden">
                 <Navbar />
                 <div
                     className="w-full h-full flex justify-center items-center"
-                    style={{ backgroundImage: `url(${Photo})`, backgroundSize: 'cover' }} 
+                    style={{ backgroundImage: `url(${Photo})`, backgroundSize: 'cover' }}
                 >
-                    <div className="w-11/12 sm:w-9/12 lg:w-1/2 h-3/4 homecard-blur drop-shadow-lg shadow-lg border-2 border-gray-100 bg-opacity-50 flex flex-col justify-center items-center rounded-md">
+                    <div className="w-[80vw]  h-3/4 homecard-blur drop-shadow-lg shadow-lg border-2 border-gray-100 bg-opacity-50 flex flex-col justify-center items-center rounded-md">
                         <p className="cursor-pointer font-bold text-2xl sm:text-3xl flex flex-col gap-1 justify-center items-center text-center">
                             <span className="flex justify-center text-stroke mb-4 text-4xl sm:text-5xl">Welcome to VerboMeet!</span>
-                            <span className="flex justify-center text-stroke2 text-lg sm:text-xl">Your Voice, Your Debate</span>
-                            <span className="flex justify-center pb-2 text-stroke2 text-lg sm:text-xl">Lock It In!</span>
+                            <span className="flex justify-center  text-lg sm:text-xl">Your Voice, Your Debate</span>
+                            <span className="flex justify-center pb-2  text-lg sm:text-xl">Lock It In!</span>
 
                             <button
-                                type="button" 
+                                type="button"
                                 onClick={handleGetStarted}
                                 className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-blue-400 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
                                 aria-label="Get Started"
                             >
                                 Get Started
                                 <svg
-                                    className="w-6 h-6 sm:w-8 sm:h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-1 sm:p-2 rotate-45"
+                                    className="w-6 h-6 sm:w-8 sm:h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full p-1 sm:p-2 rotate-45"
                                     viewBox="0 0 16 19"
                                     xmlns="http://www.w3.org/2000/svg"
                                 >
@@ -41,6 +41,7 @@ const Home = () => {
                                     ></path>
                                 </svg>
                             </button>
+
                         </p>
                     </div>
                 </div>
