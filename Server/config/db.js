29146a413ac3ac6c0f579cbd021d8cc993478fc4
Aruna456 <aruna.techbox@gmail.com>
2024-10-08 10:37:37 +0530

    Db Config changed

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,14 +1,9 @@
-// const  mongoose =require('mongoose')
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGO_URL)
 
-// mongoose.connect(process.env.MONGOURL ||"mongodb+srv://Dakku_17:dakku17@cluster0.82zvv.mongodb.net/")
+const connection = mongoose.connection;
+connection.on('connected', () => console.log("DB Connected"))
+connection.on('error', () => console.log("DB Error"))
 
-// const connection=mongoose.connection;
-// connection.on('connected',()=>{
-//     console.log("DB Created")
-// })
-
-// connection.on('error',() =>console.log("DB error"))
-
-// module.exports=mongoose
-
+module.exports = mongoose
